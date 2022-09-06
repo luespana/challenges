c4719b53ef4ca1b551bbc26686cff3268438c904
Lucía España <99997724+luespana@users.noreply.github.com>
2022-09-06 13:28:11 -0300

    #3

--- #3.js
+++ #3.js
@@ -1,23 +1,29 @@
-var input = "bici coche (balón) bici coche peluche";
+var input = ")bici( casa play";
 
 function isValid(letter) {
   var caracteres = letter.split("");
   console.log(caracteres);
   let openParenthesis = 0;
   let closeParenthesis = 0;
+  let error = false
 
   caracteres.map((caracteres) => {
     if (caracteres === "(") {
       openParenthesis += 1;
     } else if (caracteres === ")") {
-      closeParenthesis += 1;
+      if(openParenthesis !== closeParenthesis){
+        closeParenthesis += 1;
+      }
+    }
+    if(caracteres === '[' || caracteres === ']' || caracteres === '{' || caracteres === '}'){
+        error = true 
     }
   });
 
   if (
     openParenthesis === closeParenthesis &&
     openParenthesis >= 0 &&
-    closeParenthesis >= 0
+    closeParenthesis >= 0 && error === false && !letter.match(/[(]\W+[)]|[(][)]/)
   ) {
     return true;
   } else {
